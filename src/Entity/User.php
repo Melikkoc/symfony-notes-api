b7418b767daaee4b8d651a1cae081485745a6289
Melikkoc <melikkoc2007@gmail.com>
2026-01-22 11:38:25 +0100

    installed symfony/security-bundle and defined user as security-user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,13 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['email'], message: 'Email already exists')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -41,6 +43,11 @@ class User
 
         return $this;
     }
+    
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
 
     public function getPassword(): ?string
     {
@@ -56,7 +63,11 @@ class User
 
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): static
@@ -64,5 +75,9 @@ class User
         $this->roles = $roles;
 
         return $this;
+    }
+    public function eraseCredentials(): void 
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
     }
 }
